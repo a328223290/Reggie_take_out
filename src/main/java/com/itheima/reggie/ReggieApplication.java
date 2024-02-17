8b232a13d3abf8c447b0942f93def934f801c4b4
a328223290 <gexinkai2017@163.com>
2024-02-17 23:48:57 +0800

    update

--- src/main/java/com/itheima/reggie/ReggieApplication.java
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -1,10 +1,8 @@
 package com.itheima.reggie;
 
-import com.itheima.reggie.common.MailProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @Slf4j
